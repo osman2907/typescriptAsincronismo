9005304b4a6ae81b5aa74b2e38be9882dfc71b2b
Osman Pérez <operez@edutecno.com>
2023-02-10 09:49:14 -0400

    Eliminacion carpeta dist

--- dist/models/category.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AccessType = void 0;
-var AccessType;
-(function (AccessType) {
-    AccessType["PRIVATE"] = "private";
-    AccessType["PUBLIC"] = "public";
-})(AccessType = exports.AccessType || (exports.AccessType = {}));
